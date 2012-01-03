d26dd80cb87284dee1dd2492ce6ca3a3ed6411d3
criticalmass <remember0523@gmail.com>
2012-01-03 23:39:30 +0900

    basic methods for GraphDecorator

--- GraphDecorator.py
+++ GraphDecorator.py
@@ -15,12 +15,13 @@ class GraphDecorator():
     FOWARD = '|'
     BRANCH_MERGE = '<'
     BLANK = ' '
+    COMMIT = '*'
 
     def __init__(self):
         '''
         Constructor
         '''
-        self.decor = ''
+        self.decor = []
         
     def lastDecor(self):
         if len(self.decor) == 0:
@@ -54,3 +55,55 @@ class GraphDecorator():
         
     def getMaxOffset(self):
         return len(self.decor) - 1
+    
+    def getMaxBranchOffset(self):
+        for i in range(len(self.decor)-1, -1, -1):
+            d = self.decor[i]
+            if  d != GraphDecorator.MERGE and d != GraphDecorator.BLANK:
+                return i
+        return -1
+    
+    def getMaxMergeOffset(self):
+        for i in range(len(self.decor)-1, -1, -1):
+            d = self.decor[i]
+            if  d != GraphDecorator.BRANCH and d != GraphDecorator.BLANK:
+                return i
+        return -1
+        
+    
+    def assignMergeAt(self, offset):
+        #handling overflow - extend the memory
+        rest = len(self.decor) - offset
+        if rest <= 0:
+            self.decor.extend([GraphDecorator.BLANK] * (1 - rest))
+        if self.decor[offset] == GraphDecorator.BRANCH:
+            self.decor[offset] = GraphDecorator.BRANCH_MERGE
+        else:
+            self.decor[offset] = GraphDecorator.MERGE
+
+    def assignBranchAt(self, offset):
+        #handling overflow - extend the memory
+        rest = len(self.decor) - offset
+        if rest <= 0:
+            self.decor.extend([GraphDecorator.BLANK] * (1 - rest))
+        if self.decor[offset] == GraphDecorator.MERGE:
+            self.decor[offset] = GraphDecorator.BRANCH_MERGE
+        else:
+            self.decor[offset] = GraphDecorator.BRANCH
+        
+    def __str__(self):
+        return self.decor.__repr__()
+        
+if __name__ == '__main__':
+    gd = GraphDecorator()
+    print gd
+    gd.addBranch()
+    print gd
+    gd.addMerge()
+    print gd
+    gd.assignMergeAt(5)
+    print gd
+    print gd.getMaxMergeOffset()
+    print gd.getMaxBranchOffset()
+    gd.assignBranchAt(5)
+    print gd
